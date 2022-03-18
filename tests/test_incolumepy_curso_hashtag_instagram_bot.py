3ba1db6d74b3144d7681d3b0683cedd2fbd99c81
Britodfbr <britodfbr@gmail.com>
2022-03-18 10:51:40 -0300

    refactor: unificação de versionamento com pyproject.toml

--- tests/test_incolumepy_curso_hashtag_instagram_bot.py
+++ tests/test_incolumepy_curso_hashtag_instagram_bot.py
@@ -1,5 +1,18 @@
 from incolumepy_curso_hashtag_instagram_bot import __version__
+import re
+import pytest
 
 
-def test_version():
-    assert __version__ == '0.1.0'
+@pytest.mark.parametrize(
+    'version',
+    [
+        __version__,
+        '0.1.0-dev.0',
+        '0.1.0-alpha.11',
+        '0.1.0-beta.111',
+        '10.1.0-rc.111',
+    ],
+)
+def test_version(version):
+    regex = r'\d+(\.\d+){2}(-\w+\.\d+)?'
+    assert re.compile(regex).fullmatch(version)
